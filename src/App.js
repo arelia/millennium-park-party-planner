712df2b88c34335d6aa5cd0eddd57d21d3fb762b
arelia <arelia.jones@gmail.com>
2017-04-14 15:14:27 -0500

    Add clock state for practice with states

--- src/App.js
+++ src/App.js
@@ -5,9 +5,15 @@ import Event from './components/Event.js';
 import Header from './components/Header.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
   render() {
     return (
       <div className="App">
+        <p>{this.state.date.toLocaleTimeString()}</p>
         <Header />
         <Event />
       </div>
