e7609be4f648d1058af4f99fe44ac88dc7ecc6ca
Arelia Jones <arelia.jones@careerbuilder.com>
2017-06-23 14:44:21 -0500

    conver form to millenium park events

--- src/components/EventsForm.js
+++ src/components/EventsForm.js
@@ -4,39 +4,74 @@ class EventsForm extends Component {
   constructor(props){
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEventTypeChange = this.handleEventTypeChange.bind(this);
+    this.togglePriority= this.togglePriority.bind(this);
+    this.state = {
+      eventType: 'music_series',
+      priority: false
+    };
   }
 
   handleSubmit(e) {
     e.preventDefault();
 
     const newEvent = {
-      name: this.name.value,
-      date: this.date.value,
-      location: this.location.value,
-      description: this.description.value,
-      image_url: this.image_url.value
+      startDate: this.startDate.value,
+      endDate: this.endDate.value, // endDate = endDate || startDate and if endDate then multiday = true
+      startTime: this.startTime.value,
+      eventName: this.eventName.value,
+      eventType: this.eventType.value,
+      image_url: this.image_url.value,
+      priority: this.priority.value,
+      url: this.url.value,
+      description: this.description.value
     }
     this.props.addEvent(newEvent);
     this.eventForm.reset();
   }
 
+  handleEventTypeChange(e) {
+    this.setState({eventType: e.target.value});
+  }
+
+  togglePriority(e) {
+    this.setState(({ priority }) => ( { priority: !priority } ));
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit} ref={(input) => this.eventForm = input} >
-        <label htmlFor="name">Name
-          <input type="text" name="name" ref={(input) => this.name = input} />
+        <label htmlFor="startDate">Start Date
+          <input type="date" name="startDate" ref={(input) => this.startDate = input} />
         </label>
-        <label htmlFor="date">Date
-          <input type="date" name="date" min="2017-03-08" max="2017-03-19" ref={(input) => this.date = input} />
+        <label htmlFor="endDate">End Date
+          <input type="date" name="endDate" ref={(input) => this.endDate = input} />
         </label>
-        <label htmlFor="location">Location
-          <input type="text" name="location" ref={(input) => this.location = input} />
+        <label htmlFor="startTime">Start Time
+          <input type="time" name="startTime" ref={(input) => this.startTime = input} />
         </label>
-        <label htmlFor="description">Description
-          <textarea name="description" ref={(input) => this.description = input} />
+        <label htmlFor="eventName">Event Name
+          <input type="text" name="eventName" ref={(input) => this.eventName = input} />
+        </label>
+        <label htmlFor="eventType">Event Type
+          <select value={this.state.eventType} onChange={this.handleEventTypeChange}>
+            <option value="music_series">Music Series</option>
+            <option value="film_series">Film Series</option>
+            <option value="festival">Festival</option>
+            <option value="event">Event</option>
+          </select>
         </label>
         <label htmlFor="image_url">Image URL
           <input type="text" name="image_url" ref={(input) => this.image_url = input} />
+        </label>
+        <label htmlFor="url"> URL
+          <input type="text" name="url" ref={(input) => this.url = input} />
+        </label>
+        <label htmlFor="priority"> Priority
+          <input type="checkbox" name="priority" checked={this.state.priority} onChange={this.togglePriority} ref={(input) => this.priority = input} />
+        </label>
+        <label htmlFor="description">Description
+          <textarea name="description" ref={(input) => this.description = input} />
         </label>
         <input type="submit" name="submit" value="Submit" />
       </form>
