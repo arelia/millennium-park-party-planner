66b9ecd1067fe96c490481f20d9738df88d6ba78
arelia <arelia.jones@gmail.com>
2017-07-06 22:24:11 -0500

    hide events that are in the past

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import EventItem from '../components/EventItem.js';
+var moment = require('moment');
 
 class Events extends Component {
   render() {
     const eventsList = this.props.eventsList;
+    let today = moment().toISOString();
+
 
     const eventItems = Object
       .keys(eventsList)
-      .map(key =>
-        <EventItem key={key} eventInfo={eventsList[key]}/>
-      )
+      .map(key => {
+
+        let eventDate = eventsList[key].endDate || eventsList[key].startDate;
+        let eventDateObject = moment(eventDate).toISOString();
+
+        return eventDateObject > today ? <EventItem key={key} eventInfo={eventsList[key]}/> : ''
+      })
 
     return (
       <ul className="events">
