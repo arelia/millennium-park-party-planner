39b0d16998edd9190e6b8d9284eb1825bcdc850e
arelia <arelia.jones@gmail.com>
2017-07-06 22:32:00 -0500

    refactor event display logic

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,17 +5,15 @@ var moment = require('moment');
 class Events extends Component {
   render() {
     const eventsList = this.props.eventsList;
-    let today = moment().toISOString();
-
 
     const eventItems = Object
       .keys(eventsList)
       .map(key => {
 
         let eventDate = eventsList[key].endDate || eventsList[key].startDate;
-        let eventDateObject = moment(eventDate).toISOString();
+        let displayEvent = moment(eventDate).isAfter(moment());
 
-        return eventDateObject > today ? <EventItem key={key} eventInfo={eventsList[key]}/> : ''
+        return displayEvent ? <EventItem key={key} eventInfo={eventsList[key]}/> : null
       })
 
     return (
